185ad8cb59df3cdcd57bed816faa36913c7deac2
Erin King <eohone@gmail.com>
2019-01-05 09:49:34 -0700

    added Rakefile, moved .gitignore to git folder, test_it_exists, test_it_encrypts_with_key_and_date and encrypt

---
+++ lib/enigma.rb
@@ -0,0 +1,18 @@
+require 'pry'
+require 'date'
+
+class Enigma
+  def initialize
+
+  end
+
+  def encrypt(message, key, date)
+    # returns a hash
+    # {
+    #   encryption: "", #encrypted message
+    #   key: "",
+    #   date: ""
+    # }
+  end
+
+end
---
+++ test/enigma_test.rb
@@ -0,0 +1,56 @@
+require 'pry'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/enigma'
+
+class EnigmaTest < Minitest::Test
+
+  def setup
+    @enigma = Enigma.new
+
+  end
+
+  def test_it_exists
+
+    assert_instance_of Enigma, @enigma
+  end
+
+  def test_it_encrypts_with_key_and_date
+
+    expected =
+      {
+        encryption: "keder ohulw",
+        key: "02715",
+        date: "040895"
+      }
+
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  end
+
+end
+
+# # encrypt a message with a key and date
+# pry(main)>
+# #=>
+
+#
+# # decrypt a message with a key and date
+# pry(main) > enigma.decrypt("keder ohulw", "02715", "040895")
+# #=>
+# #   {
+# #     decryption: "hello world",
+# #     key: "02715",
+# #     date: "040895"
+# #   }
+#
+# # encrypt a message with a key (uses today's date)
+# pry(main)> encrypted = enigma.encrypt("hello world", "02715")
+# #=> # encryption hash here
+#
+# #decrypt a message with a key (uses today's date)
+# pry(main) > enigma.decrypt(encrypted[:encryption], "02715")
+# #=> # decryption hash here
+#
+# # encrypt a message (generates random key and uses today's date)
+# pry(main)> enigma.encrypt("hello world")
+# #=> # encryption hash here
