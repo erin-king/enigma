49a61e9d7e4e66ee4be83222bfe1ba9d46fb25e9
Erin King <eohone@gmail.com>
2019-01-07 16:55:13 -0700

    added initialize method for Shift class, changed test_it_exists to take keys and offset objects in Shift object arguments

---
+++ lib/shift.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+class Shift
+  def initialize(key, offset)
+    @key = key
+    @offset = offset
+  end
+
+
+end
+
+#methods shift_a, etc
+#return a...z "" array
+#
--- test/shift_test.rb
+++ test/shift_test.rb
@@ -1,12 +1,16 @@
 require 'pry'
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/keys'
+require './lib/offset'
 require './lib/shift'
 
 class ShiftTest < Minitest::Test
   def test_it_exists
-    shift_a = Shift.new(key, offset)
-    
-    assert_instance_of Shift, shift_a
+    key = Keys.new("12345")
+    offset = Offset.new("291182")
+    shift = Shift.new(key, offset)
+
+    assert_instance_of Shift, shift
   end
 end
